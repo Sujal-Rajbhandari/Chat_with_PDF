f65c7fd9fb248e5d301121c34300f30a4107f2c8
Sujal-Rajbhandari <sujal.rajbhandari@westcliff.edu>
2025-04-21 13:11:26 +0545

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,18 +1,4 @@
-# import streamlit as st
-# import main  # Assuming main.py contains the code from above
 
-# st.title("Chat with PDFs using Deepseek")
-# uploaded_file = st.file_uploader("Upload PDF", type="pdf")
-
-# if uploaded_file:
-#     main.upload_pdf(uploaded_file)
-#     file_path = f"{main.pdfs_directory}/{uploaded_file.name}"
-#     db = main.create_vector_store(file_path)
-#     question = st.text_input("Ask a question about the PDF:")
-#     if question:
-#         related_documents = main.retrieve_docs(db, question)
-#         answer = main.question_pdf(question, related_documents)
-#         st.write(answer)
 import streamlit as st
 import main as main
 
